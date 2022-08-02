00e5737c0e07976da1a0cfd95ad2b86a1a32a68d
Priyanka Tyagi <udittyagi455@gmail.com>
2022-08-02 14:49:54 +0530

    completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule} from "@angular/common/http";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { EmplistComponent } from './components/emplist/emplist.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EmplistComponent
   ],
   imports: [
     BrowserModule,
